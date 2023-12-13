0d522e7f6e72ea315e6d242363e47374ff24b004
Ana Beatriz <nabiamarini@gmail.com>
2023-12-12 21:57:10 -0300

    feat: pagina para adicionar uma nova empresa

--- Controllers/EmpresasController.cs
+++ Controllers/EmpresasController.cs
@@ -16,22 +16,56 @@ public class EmpresasController : Controller
 {
 
     public readonly string uriBase = "http://myprojects.somee.com/CnpjApi/Empresas";
-    
+
     [HttpGet]
     public async Task<ActionResult> IndexAsync()
+    {
+        try
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync(uriBase);
+            string serialized = await response.Content.ReadAsStringAsync();
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                List<EmpresasViewModel> listaEmpresas = await Task.Run(() =>
+                    JsonConvert.DeserializeObject<List<EmpresasViewModel>>(serialized));
+
+                return View(listaEmpresas);
+            }
+            else
+                throw new System.Exception(serialized);
+        }
+        catch (System.Exception ex)
+        {
+            TempData["MensagemErro"] = ex.Message;
+            return RedirectToAction("Index");
+        }
+    }
+
+    [HttpGet]
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateAsync(EmpresasViewModel novaEmpresa)
         {
             try
             {
                 HttpClient httpClient = new HttpClient();
-                HttpResponseMessage response = await httpClient.GetAsync(uriBase);
+                string uricomplementar = "/Adicionar";
+                var content = new StringContent(JsonConvert.SerializeObject(novaEmpresa));
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                HttpResponseMessage response = await httpClient.PostAsync(uriBase + uricomplementar, content);
+
                 string serialized = await response.Content.ReadAsStringAsync();
 
                 if (response.StatusCode == System.Net.HttpStatusCode.OK)
                 {
-                    List<EmpresasViewModel> listaEmpresas = await Task.Run(() =>
-                        JsonConvert.DeserializeObject<List<EmpresasViewModel>>(serialized));
-
-                    return View(listaEmpresas);
+                    TempData["Mensagem"] = string.Format("Empresa {0} foi adicionada com sucesso!", novaEmpresa.Nome);
+                    return RedirectToAction("Index", "Empresas");
                 }
                 else
                     throw new System.Exception(serialized);
@@ -39,10 +73,10 @@ public class EmpresasController : Controller
             catch (System.Exception ex)
             {
                 TempData["MensagemErro"] = ex.Message;
-                return RedirectToAction("Index");
+                return RedirectToAction("Create");
             }
         }
-
+        
 
 
 
